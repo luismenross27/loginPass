8a5abb3693cc61a845f4f35e1a63609a9c5135a0
luismenross27 <112197656+luismenross27@users.noreply.github.com>
2023-10-16 13:35:51 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
     
     try{
-         $conexion = new PDO('mysql:host=localhost;dbname=login_tuto', 'josejaime', 'admin1234');
+         $conexion = new PDO('mysql:host=localhost;dbname=db', 'db', 'AVNS_SkDFGosTY1m0cEqfxD7');
     }catch(PDOException $prueba_error){
         echo "Error: " . $prueba_error->getMessage();
     }
